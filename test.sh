a3e4babdc42f479284b5e92a118e3b4e46c6966d
ballm <martin.ball@grg.com>
2016-11-11 10:21:41 +0000

    updates to work with eap 7.0.3

--- test.sh
+++ test.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-a=$1
-b=$2
-# Lexicographic (greater than, less than) comparison.
-if [ "$a" -ge "$b" ]; then
-    echo "$a is lexicographically smaller or equal to $b"
-elif [ "$a" -le "$b" ]; then
-    echo "$b is lexicographically smaller than $a"
-else
-    echo "Strings are equal"
-fi
+REAL_CORE_EAP_VERSION="2.1.8.Final-redhat-1"
+if [ -f src/wildfly-core-$REAL_CORE_EAP_VERSION.patch ]
+        then
+            echo "Patching core files"
+            echo "=== Patch Core ===" >> work/build.log
+            patch -v -p0 < src/wildfly-core-$REAL_CORE_EAP_VERSION.patch
+        fi
